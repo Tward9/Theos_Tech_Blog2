469708dc73138ca954d6cffd1e4a6569949f0b88
Theo Ward <theoward9@gmail.com>
2022-05-03 16:35:24 -0500

    adding login logout js, setting up homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const dashboardRoutes = require('./dashboard-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
